bad878fe72ab6871e3e442b0a9d6f1f7ce543b85
Broto <brotoo25@gmail.com>
2020-02-15 18:30:17 -0300

    Use Task await function as default - Add Flow implementation for queries

--- firestore-coroutines/src/main/kotlin/com/kiwimob/firestore/coroutines/QueryCoroutines.kt
+++ firestore-coroutines/src/main/kotlin/com/kiwimob/firestore/coroutines/QueryCoroutines.kt
@@ -1,80 +1,27 @@
 package com.kiwimob.firestore.coroutines
 
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.sendBlocking
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
+import kotlinx.coroutines.flow.callbackFlow
 
-suspend fun <T : Any> Query.await(clazz: Class<T>): List<T> {
-    return await { documentSnapshot -> documentSnapshot.toObject(clazz) as T }
-}
-
-suspend fun <T : Any> Query.awaitSingle(clazz: Class<T>): T {
-    return awaitSingle { documentSnapshot -> documentSnapshot.toObject(clazz) as T }
-}
-
-suspend fun Query.await(): QuerySnapshot {
-    return suspendCancellableCoroutine { continuation ->
-        get().addOnCompleteListener {
-            if (it.isSuccessful && it.result != null) {
-                continuation.resume(it.result!!)
-            } else {
-                continuation.resumeWithException(it.exception ?: IllegalStateException())
-            }
-        }
-    }
-}
-
-suspend fun <T : Any> Query.await(parser: (documentSnapshot: DocumentSnapshot) -> T): List<T> {
-    return suspendCancellableCoroutine { continuation ->
-        get().addOnCompleteListener {
-            if (it.isSuccessful && it.result != null) {
-                val list = it.result!!.map(parser)
-                continuation.resume(list)
-            } else {
-                continuation.resumeWithException(it.exception ?: IllegalStateException())
-            }
-        }
-    }
-}
-
-suspend fun <T : Any> Query.awaitSingle(parser: (documentSnapshot: DocumentSnapshot) -> T): T {
-    return suspendCancellableCoroutine { continuation ->
-        get().addOnCompleteListener {
-            if (it.isSuccessful && it.result != null) {
-                continuation.resume(parser(((it.result) as QuerySnapshot).documents[0]))
-            } else {
-                continuation.resumeWithException(it.exception ?: IllegalStateException())
-            }
-        }
-    }
-}
-
-fun <T : Any> Query.observe(clazz: Class<T>): ReceiveChannel<List<T>> = observe { documentSnapshot ->
-    documentSnapshot.toObject(clazz) as T
-}
-
-fun <T : Any> Query.observe(parser: (documentSnapshot: DocumentSnapshot) -> T): ReceiveChannel<List<T>> {
-    val channel = Channel<List<T>>()
-
-    addSnapshotListener { querySnapshot, exception ->
+@ExperimentalCoroutinesApi
+fun Query.snapshotAsFlow() = callbackFlow<QuerySnapshot> {
+    val registration = addSnapshotListener { querySnapshot, exception ->
         exception?.let {
-            channel.close(it)
-            return@addSnapshotListener
+            close(it)
         }
         if (querySnapshot == null) {
-            channel.close()
-            return@addSnapshotListener
+            close()
         }
-
-        val list = querySnapshot.map(parser)
-        channel.sendBlocking(list)
+        querySnapshot?.let {
+            sendBlocking(it)
+        }
     }
 
-    return channel
+    awaitClose {
+        registration.remove()
+    }
 }
--- firestore-coroutines/src/main/kotlin/com/kiwimob/firestore/coroutines/TaskCoroutines.kt
+++ firestore-coroutines/src/main/kotlin/com/kiwimob/firestore/coroutines/TaskCoroutines.kt
@@ -2,12 +2,24 @@ package com.kiwimob.firestore.coroutines
 
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-private suspend fun <T> Task<T>.await(): T {
+suspend inline fun <reified T : Any> Task<T>.await(): T {
     return suspendCancellableCoroutine { continuation ->
-        addOnSuccessListener(continuation::resume)
+        addOnCompleteListener {
+            if (it.isSuccessful) {
+                if (it.result != null) {
+                    continuation.resume(it.result!!)
+                } else {
+                    continuation.resumeWithException(EmptyStackException())
+                }
+            } else {
+                continuation.resumeWithException(it.exception ?: IllegalStateException())
+            }
+        }
+        addOnSuccessListener { continuation.resume(it) }
         addOnFailureListener { continuation.resumeWithException(it) }
         addOnCanceledListener { continuation.cancel() }
     }
