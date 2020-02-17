edf00867c54ad69d4b56b5f6ca6c6344c04f98e4
Broto <brotoo25@gmail.com>
2020-02-16 23:07:31 -0300

    Remove Task onComplete implementation

--- firestore-coroutines/src/main/kotlin/com/kiwimob/firestore/coroutines/TaskCoroutines.kt
+++ firestore-coroutines/src/main/kotlin/com/kiwimob/firestore/coroutines/TaskCoroutines.kt
@@ -2,23 +2,11 @@ package com.kiwimob.firestore.coroutines
 
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.suspendCancellableCoroutine
-import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
 suspend inline fun <reified T : Any> Task<T>.await(): T {
     return suspendCancellableCoroutine { continuation ->
-        addOnCompleteListener {
-            if (it.isSuccessful) {
-                if (it.result != null) {
-                    continuation.resume(it.result!!)
-                } else {
-                    continuation.resumeWithException(EmptyStackException())
-                }
-            } else {
-                continuation.resumeWithException(it.exception ?: IllegalStateException())
-            }
-        }
         addOnSuccessListener { continuation.resume(it) }
         addOnFailureListener { continuation.resumeWithException(it) }
         addOnCanceledListener { continuation.cancel() }
